47bbcc04eff4ecb1d8f9f53a99a5cc97a11b843c
sbs-blip <sbs.330@gmail.com>
2019-12-06 11:00:50 -0800

    commit number 2

--- test_project.R
+++ test_project.R
@@ -1,1 +1,15 @@
 # Test scritp R studio to Github
+
+# 1. save the changes in R
+# 2. the file will show up in the GIt tab
+# 3. Select the file and push
+# 4. commit with a comment
+# 5. Should now be on Git!
+
+#push 
+
+# more changes that aren't being pushed
+
+# change 
+
+# another change
